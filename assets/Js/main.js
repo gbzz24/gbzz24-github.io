58d466e7f6d279429cbc065c58509d2b1996145a
gbzz24 <gabrielsuarez24@gmail.com>
2021-07-19 00:44:37 -0300

    subida

---
+++ assets/Js/main.js
@@ -0,0 +1,63 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  $(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+       isClosed = false;
+  
+      trigger.click(function () {
+        hamburger_cross();      
+      });
+  
+      function hamburger_cross() {
+  
+        if (isClosed == true) {          
+          overlay.hide();
+          trigger.removeClass('is-open');
+          trigger.addClass('is-closed');
+          isClosed = false;
+        } else {   
+          overlay.show();
+          trigger.removeClass('is-closed');
+          trigger.addClass('is-open');
+          isClosed = true;
+        }
+    }
+    
+    $('[data-toggle="offcanvas"]').click(function () {
+          $('#menu').toggleClass('toggled');
+    });  
+  });
+
+  document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
+/*-------------------------------------------------------------------------------------*/
+/*---------------------------------------MENU---------------------------------------------------*/
+$('button').on('click', function(){
+  $('body').toggleClass('open');
+});
+/*---------------------------------------MENU---------------------------------------------------*/
+
+/*---------------------------------------SKILLS---------------------------------------------------*/
+$('.card').click(function(){
+  $(this).toggleClass('flipped');
+});
+/*---------------------------------------SKILLS---------------------------------------------------*/
---
+++ assets/Js/pages.js
@@ -0,0 +1,88 @@
+/*------------------------------------ ABOUT -------------------------------------------------*/
+let sections = gsap.utils.toArray("section"),
+    currentSection = sections[3];
+
+gsap.defaults({overwrite: 'auto', duration: 0.3});
+
+// stretch out the body height according to however many sections there are. 
+gsap.set("body", {height: (sections.length * 100) + "%"});
+
+// create a ScrollTrigger for each section
+sections.forEach((section, i) => {
+  ScrollTrigger.create({
+    // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
+    start: () => (i - 0.5) * innerHeight,
+    end: () => (i + 0.5) * innerHeight,
+    // when a new section activates (from either direction), set the section accordinglyl.
+    onToggle: self => self.isActive && setSection(section)
+  });
+});
+
+function setSection(newSection) {
+  if (newSection !== currentSection) {
+    gsap.to(currentSection, {scale: 0.8, autoAlpha: 0})
+    gsap.to(newSection, {scale: 1, autoAlpha: 1});
+    currentSection = newSection;
+  }
+}
+
+// handles the infinite part, wrapping around at either end....
+ScrollTrigger.create({
+  start: 1,
+  end: () => ScrollTrigger.maxScroll(window) - 1,
+  onLeaveBack: self => self.scroll(ScrollTrigger.maxScroll(window) - 2),
+  onLeave: self => self.scroll(2)
+}).scroll(2);
+
+
+/*---------------------------------------MENU---------------------------------------------------*/
+$('button').on('click', function(){
+  $('body').toggleClass('open');
+});
+/*---------------------------------------MENU---------------------------------------------------*/
+/*---------------------------------------WORKS---------------------------------------------------*/
+
+function animateFrom(elem, direction) {
+  direction = direction || 1;
+  var x = 0,
+      y = direction * 100;
+  if(elem.classList.contains("gs_reveal_fromLeft")) {
+    x = -100;
+    y = 0;
+  } else if (elem.classList.contains("gs_reveal_fromRight")) {
+    x = 100;
+    y = 0;
+  }
+  elem.style.transform = "translate(" + x + "px, " + y + "px)";
+  elem.style.opacity = "0";
+  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+    duration: 1.25, 
+    x: 0,
+    y: 0, 
+    autoAlpha: 1, 
+    ease: "expo", 
+    overwrite: "auto"
+  });
+}
+
+function hide(elem) {
+  gsap.set(elem, {autoAlpha: 0});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  gsap.registerPlugin(ScrollTrigger);
+  
+  gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+    hide(elem); // assure that the element is hidden when scrolled into view
+    
+    ScrollTrigger.create({
+      trigger: elem,
+      onEnter: function() { animateFrom(elem) }, 
+      onEnterBack: function() { animateFrom(elem, -1) },
+      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+    });
+  });
+});
+
+/*---------------------------------------PRODUCTS---------------------------------------------------*/
+
